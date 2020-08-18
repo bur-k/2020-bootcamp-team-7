9a9b768fac5fe8dbe0e0f664bb3a4e8254442311
oguzmertcaylar <oguzmertcaylar7@gmail.com>
2020-08-18 14:42:51 +0300

    added createUser reducer definitions

--- frontend-react-boilerplate/app/containers/MyAccount/reducer.js
+++ frontend-react-boilerplate/app/containers/MyAccount/reducer.js
@@ -9,10 +9,14 @@ import {
   PULL_ACCOUNT,
   PULL_ACCOUNT_SUCCESS,
   PULL_ACCOUNT_ERROR,
+  CREATE_USER,
+  CREATE_USER_SUCCESS,
+  CREATE_USER_ERROR
 } from './constants';
 
 export const initialState = {
   user: null,
+  //userId: null
 };
 
 /* eslint-disable default-case, no-param-reassign */
@@ -22,12 +26,25 @@ const accountReducer = (state = initialState, action) =>
       case DEFAULT_ACTION:
         break;
       case PULL_ACCOUNT:
+        //draft.userId = action.userId;
+        draft.getUserData = action.data;
         break;
       case PULL_ACCOUNT_SUCCESS:
-        draft.user = action.user;
+        //draft.userData = action.data;
+        draft.getUserResponse = action.data;
         break;
       case PULL_ACCOUNT_ERROR:
-        draft.error = action.error;
+        //draft.error = action.error;
+        draft.getUserError = action.error;
+        break;
+      case CREATE_USER:
+        draft.createUserData = action.data;
+        break;
+      case CREATE_USER_SUCCESS:
+        draft.createUserRespose = action.data;
+        break;
+      case CREATE_USER_ERROR:
+        draft.createUserError = action.error;
         break;
     }
   });
