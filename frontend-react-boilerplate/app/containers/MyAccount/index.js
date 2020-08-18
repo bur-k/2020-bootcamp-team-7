e7e1dc546ff56dd0c4e5f746daf8daa23412a849
oguzmertcaylar <oguzmertcaylar7@gmail.com>
2020-08-18 14:48:24 +0300

    added dispatch for createUser and added prototype for profile visualization

--- frontend-react-boilerplate/app/containers/MyAccount/index.js
+++ frontend-react-boilerplate/app/containers/MyAccount/index.js
@@ -18,32 +18,69 @@ import makeSelectUserDetails, {
 } from './selectors';
 import reducer from './reducer';
 import saga from './saga';
-import { pullAccount } from './actions';
-
-export function UserDetails({ onLoadUser }) {
+import { pullAccount,createUser } from './actions';
+import { Card, Button, InputGroup, FormControl, Image } from 'react-bootstrap';
+import './style.css'; 
+export function UserDetails({ 
+  onLoadUser, 
+  createUser
+}) {
   useInjectReducer({ key: 'userDetails', reducer });
   useInjectSaga({ key: 'userDetails', saga });
 
   useEffect(() => {
-    onLoadUser();
+    if (global.e && global.e.user) {
+      createUser(global.e);
+    }
+    onLoadUser(global);
   }, []);
+
+  return (
+    <div style={{ height: '100%' }}>
+      <div className="container-center">
+        <Image className="profile-pic" variant="top" src={`${onLoadUser.profilePhoto}`} style={{ width: '30%', height: '30%' }} />
+      </div>
+      <Card style={{ width: '80%', height:'100%', marginRight: 'auto', marginLeft: 'auto', marginTop: '-10%', zIndex: '-1' }}>
+      <Card.Body>
+      <div style={{ width: '30%', height: '70%', zIndex: '0' }}> 
+      <InputGroup>
+        <InputGroup.Prepend>
+          <InputGroup.Text>BIO</InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl as="textarea" aria-label="With textarea" >
+          hello
+        </FormControl>
+      </InputGroup>
+      </div>
+
+      </Card.Body>
+    </Card>
+
+      
+    </div>
+  );
 }
 
 UserDetails.propTypes = {
   dispatch: PropTypes.func.isRequired,
   user: PropTypes.object,
+  onLoadUser: PropTypes.func,
 };
 
 const mapStateToProps = createStructuredSelector({
   userDetails: makeSelectUserDetails(),
-  user: makeSelectUser(),
+  //user: makeSelectUser(),
+  //uid: makeSelectUid(),
 });
 
 function mapDispatchToProps(dispatch) {
   return {
     dispatch,
-    onLoadUser: () => {
-      dispatch(pullAccount());
+    onLoadUser: (data) => {
+      dispatch(pullAccount(data));
+    },
+    createUser: (data) => {
+      dispatch(createUser(data));
     },
   };
 }
